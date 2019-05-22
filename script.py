07981ffde71b5c510720f05d91ff180267680f7b
elad-testing <50847355+elad-testing@users.noreply.github.com>
2019-05-22 23:38:28 +0300

    Update script.py

--- script.py
+++ script.py
@@ -5,5 +5,6 @@ for _ in range(9):
   print('wait for something')
   time.sleep(60 * 5)
 '''
-print('my sha is: %s' % SHA)
+print('my sha is: %s' % os.environ['SHA'])
+print('my sha is: %s' % os.environ['$TRAVIS_PULL_REQUEST_SHA'])
 print('finished')
