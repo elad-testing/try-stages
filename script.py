9c04ac14160854c251a4853b2a1b6aeda52f61b8
elad-testing <50847355+elad-testing@users.noreply.github.com>
2019-05-25 12:47:06 +0300

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,17 @@ for _ in range(9):
   print('wait for something')
   time.sleep(60 * 5)
 '''
+
+print("timeout:" + os.environ['TIMEOUT'])
+
+os.putenv("TIMEOUT", "42")
+
+print("timeout after change:" + os.environ['TIMEOUT'])
+
+
+print("This is what I have: \n" + os.environ)
+
+
 print('my sha is: %s' % os.environ['SHA'])
-print('my sha is: %s' % os.environ['$TRAVIS_PULL_REQUEST_SHA'])
+print('my pr is: %s' % os.environ['$TRAVIS_PULL_REQUEST_SHA'])
 print('finished')
